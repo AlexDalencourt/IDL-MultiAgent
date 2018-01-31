ede931805fa77624c07c9556b5fecf88fe2712f2
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2018-01-31 16:02:43 +0100

    Add avatar agent

---
+++ src/main/java/multiAgent/avatar/Avatar.java
@@ -0,0 +1,44 @@
+package multiAgent.avatar;
+
+import java.awt.Graphics;
+
+import multiAgent.core.Agent;
+import multiAgent.core.Environnement;
+
+public class Avatar extends Agent {
+
+	public Avatar(int posX, int posY, Environnement env) {
+		super(posX, posY, env);
+	}
+
+	@Override
+	public void decide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drawAgent(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getNewPosX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNewPosY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
