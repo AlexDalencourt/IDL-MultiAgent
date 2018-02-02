6165f9dcdaf33ecbbd147f0ae6c31e88fdd4889d
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2018-02-02 13:57:38 +0100

    Start of dijkstra
    need to restack new pos

--- src/main/java/multiAgent/avatar/Avatar.java
+++ src/main/java/multiAgent/avatar/Avatar.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import multiAgent.ConstantParams;
 import multiAgent.core.Agent;
@@ -14,6 +16,8 @@ public class Avatar extends CommonAgentBehavour implements KeyListener {
 	private int dirX = 0;
 	
 	private int dirY = 0;
+	
+	private int[][] dijkstra;
 	
 	public Avatar(int posX, int posY, Environnement env) {
 		super(posX, posY, env);
@@ -27,6 +31,12 @@ public class Avatar extends CommonAgentBehavour implements KeyListener {
 		env.applyTransition(this);
 	}
 
+	private List<Long> stack = new ArrayList<>();
+
+	private static final int binaryMask = 0b11111111111111111111;
+	
+	private static final int hexaShift = 0x34;
+	
 	@Override
 	public void update() {
 		int futurX = posX + dirX;
@@ -39,6 +49,44 @@ public class Avatar extends CommonAgentBehavour implements KeyListener {
 		}
 		posX  = futurX;
 		posY = futurY;
+		recalculDijkstra();
+	}
+
+	private void recalculDijkstra() {
+		dijkstra = new int [ConstantParams.getGridSizeX()][ConstantParams.getGridSizeY()];
+		long currPosition = (((long)posX) << hexaShift) + ((long)posY);
+		int currentValue = 0;
+		int calculatePosX;
+		int calculatePosY;
+		dijkstra[posX][posY] = -1;
+		for(int i = -1; i < 2; i++) {
+			for(int j = -1; j < 2; j++) {
+				calculatePosX = posX + i;
+				calculatePosY = posY + i;
+				if(//env.checkOutOfBorders(calculatePosX, calculatePosY) && 
+						((CommonAgentBehavour)env.getCell(calculatePosX, calculatePosY)).canGoOn()) {
+					stack.add((long) ((calculatePosX << hexaShift) + calculatePosY));
+					dijkstra[calculatePosX][calculatePosY] = 1;
+				}
+			}
+		}
+		while(!stack.isEmpty()) {
+			currPosition = stack.remove(0);
+			calculatePosX = (int)(currPosition >> hexaShift);
+			calculatePosY = (int)(currPosition & binaryMask);
+			currentValue = dijkstra[calculatePosX][calculatePosY];
+			for(int i = -1; i < 2; i++) {
+				for(int j = -1; j < 2; j++) {
+					if(//env.checkOutOfBorders(calculatePosX + i, calculatePosY) &&
+							((CommonAgentBehavour)env.getCell(calculatePosX + i, calculatePosY + j)).canGoOn() &&
+							dijkstra[calculatePosX + i][calculatePosY + j] > currentValue + 1){
+						dijkstra[calculatePosX + i][calculatePosY + j] = currentValue + 1;
+					}
+					
+					//Stack new pos
+				}
+			}
+		}
 	}
 
 	@Override
