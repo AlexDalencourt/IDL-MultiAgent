688b0ce52e84e07cf7ffc76edb36345193a4c536
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2018-02-04 14:27:24 +0100

    debugger dijkstra

--- src/main/java/multiAgent/avatar/CommonAgentBehavour.java
+++ src/main/java/multiAgent/avatar/CommonAgentBehavour.java
@@ -1,5 +1,9 @@
 package multiAgent.avatar;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
+import multiAgent.ConstantParams;
 import multiAgent.core.Agent;
 import multiAgent.core.Environnement;
 
@@ -10,4 +14,11 @@ public abstract class CommonAgentBehavour extends Agent {
 	}
 
 	public abstract boolean canGoOn();
+
+	@Override
+	public void drawAgent(Graphics g) {
+		int pixelPosX = ConstantParams.getBoxSize() * posX;
+		int pixelPosY = ConstantParams.getBoxSize() * posY;
+		g.fillRect(pixelPosX, pixelPosY, ConstantParams.getBoxSize(), ConstantParams.getBoxSize());
+	}
 }
